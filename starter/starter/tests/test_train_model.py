d57898e58b5ca1696dbc59ad3f13bbd025b6df8e
MaxenceQueyrel <maxxxqueyrel@gmail.com>
2021-11-03 12:32:00 +0100

    connect to aws and improve fixture

--- starter/starter/tests/test_train_model.py
+++ starter/starter/tests/test_train_model.py
@@ -10,13 +10,32 @@ import boto3
 path_model = os.path.abspath(os.path.join(__file__, os.path.pardir, "model.pkl"))
 path_scores = os.path.abspath(os.path.join(__file__, os.path.pardir,  "scores.csv"))
 
+
 # Arrange
-@pytest.fixture
+@pytest.fixture(scope="session")
 def data():
-    path_data = os.path.abspath(os.path.join(__file__, *[os.path.pardir]*3, "data/census_clean.csv"))
-    data = pd.read_csv(path_data)
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+    )
+    bucket = "udacity-exercice-3"
+    prefix = 'data/12/c208530a5680c15ae19b34152286dd'
+    response = s3.get_object(Bucket=bucket, Key=prefix)
+    status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+    assert status == 200, f"Unsuccessful S3 get_object response. Status - {status}"
+    data = pd.read_csv(response.get("Body"))
+    # path_data = os.path.abspath(os.path.join(__file__, *[os.path.pardir] * 3, "data/census_clean.csv"))
+    # data = pd.read_csv(path_data)
     data_processed, y_data, encoder = train_model.process_data(data, categorical_features=train_model.cat_features, label=train_model.label, training=True)
     return data_processed, y_data
+
+
+@pytest.fixture(scope="session", autouse=True)
+def remove():
+    yield
+    os.remove(path_model)
+    os.remove(path_scores)
 
 
 def test_process_data(data):
@@ -40,4 +59,10 @@ def test_evaluate_model(data):
     score = train_model.evaluate_model(clf, data_processed, y_data, path_scores)
     assert score >= 0. and score <= 1.
     assert os.path.exists(path_scores)
+
+
+
+
+
+
 
